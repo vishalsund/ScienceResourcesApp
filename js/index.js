0927b42134891386a7e6cdaf2e7daa1c030fb36d
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-12-18 21:08:06 -0600

    Made icons smaller and made it so clicking on a section redirects

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("chemistry-section").onclick = () => {
+    window.location.href = "./chemistry/home.html"
+}
+
+document.getElementById("biology-section").onclick = () => {
+    window.location.href = "./biology/home.html"
+}
+
+document.getElementById("physics-section").onclick = () => {
+    window.location.href = "./physics/home.html"
+}
