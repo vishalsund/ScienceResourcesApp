97ee464ab8b88d8d7a6dc7f5ef12f24a2955a51b
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-12-20 14:42:05 -0600

    Set up scss pages

--- cscss.bat
+++ cscss.bat
@@ -1,4 +1,9 @@
 @echo off
 sass scss/index.scss css/index.css
 sass scss/quiz.scss css/quiz.css
-sass scss/colors.scss css/colors.css
+sass scss/section-home-page.scss css/section-home-page.css 
+
+@REM themes
+sass scss/themes/biology.scss css/themes/biology.css 
+sass scss/themes/chemistry.scss css/themes/chemistry.css 
+sass scss/themes/physics.scss css/themes/physics.css 
