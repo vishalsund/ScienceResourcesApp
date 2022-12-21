3458ed66cb2195904777556104a20bd1b476743c
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-12-20 22:22:08 -0600

    Made the home sections able to use links

---
+++ js/section-page.js
@@ -0,0 +1,7 @@
+const sect1 = document.getElementById("section1")
+const sect2 = document.getElementById("section2")
+const sect3 = document.getElementById("section3")
+
+sect1.onclick = () => window.location.href = sect1.dataset.link
+sect2.onclick = () => window.location.href = sect2.dataset.link
+sect3.onclick = () => window.location.href = sect3.dataset.link
