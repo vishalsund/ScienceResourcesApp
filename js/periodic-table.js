3d408476ecdbb84df515d57649011694aa0b3258
ShadowStryk3r <vishalsn08@gmail.com>
2022-12-21 10:29:38 -0600

    fixed stuff

--- js/periodic-table.js
+++ js/periodic-table.js
@@ -1,6 +1,20 @@
-var type = $('#periodictables').find(":selected").val();
-const image = document.getElementById('periodictable');
-const dropdown = document.getElementById('dropdown');
-image.addEventListener('click', function handleClick(){
-    image.src="../img/"+type+".png";
-});
+var pictureList = [
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Chemical_Group_Block_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Standard_State_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Atomic_Mass_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Electron_Configuration_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Oxidation_States_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Electronegativity_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Atomic_Radius_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Ionization_Energy_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Electron_Affinity_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Melting_Point_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Boiling_Point_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Density_PubChem.png",
+    "img/Periodic Tables/Periodic_Table_of_Elements_w_Year_Discovered_PubChem.png",
+];
+function switchImage() {
+    var selection = document.getElementById("periodictables");
+    var chosen = selection.value;
+    document.getElementById("periodictable").src = "../"+pictureList[chosen];
+};
