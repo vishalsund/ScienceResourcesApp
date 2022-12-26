bd32be4b5c45270fd0938246a051b0cb17b4e01d
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-12-26 13:31:10 -0600

    Made the homepage more responsive and added a very simple 404

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-let pickedSection
+let pickedSection = null
 
 const chemSect = document.getElementById("chem-section")
 const bioSect = document.getElementById("bio-section")
 const phySect = document.getElementById("phy-section")
 
+let inAnim = false
+let animId = 0
+
 function swap(div)
 {
     div.onclick = () => {
+        inAnim = true
         if (pickedSection)
             pickedSection.classList.remove("selected")
     
@@ -17,6 +21,13 @@ function swap(div)
             div.classList.add("selected")
             pickedSection = div
         }
+        animId++;
+        let currentAnim = animId
+
+        setTimeout(() =>{
+            if (animId == currentAnim)
+                inAnim = false
+        }, 500)
     }
 }
 
@@ -27,8 +38,8 @@ swap(phySect)
 const subSects = document.getElementsByClassName("sub-sect")
 for (let i = 0; i < subSects.length; i++)
 {
-    // subSects.item(i)
     subSects.item(i).onclick = () => {
+        if (inAnim || pickedSection == null) {return}
         window.location.href = subSects.item(i).dataset.link
     }
 }
